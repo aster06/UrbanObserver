6aad03f7ad19781e09f2c0c09897987ed1b5a078
Bohdana Ostapiuk <danaostapiuk753@gmail.com>
2024-02-23 15:34:53 +0200

    #7452: Created blocks for navigation and implementing components from story book

--- web/themes/custom/urbanobserver/components/02-molecules/menus/navigation/navigation-menu.js
+++ web/themes/custom/urbanobserver/components/02-molecules/menus/navigation/navigation-menu.js
@@ -1,31 +1,37 @@
 Drupal.behaviors.mainMenu = {
   attach(context) {
-    const toggleExpand = context.getElementById('toggle-expand');
-    const menu = context.getElementById('navigation-nav');
-    const logo = context.getElementById('nav-logo');
-    const littlePreMenu = context.getElementById('lower-block');
-    if (menu) {
-      const expandMenu = menu.getElementsByClassName('expand-sub');
+    const toggleExpand = once('toggle-expand', '#toggle-expand', context);
+    const menu = once('navigation-nav', '#navigation-nav', context);
+    const logo = once('nav-logo', '#nav-logo', context);
+    const littlePreMenu = once('lower-block', '.little-pre-menu', context);
+    const searchIcon = once('search-icon', '#search-icon', context);
+    const searchForm = once('search-block-form', '#block-urbanobserver-search-form-wide', context);
+    const pricingButton = once('see-pricing', '.button-see-pricing', context);
+    const socialLinks = once('social-links', '#social-links', context);
 
-      // Mobile Menu Show/Hide.
-      toggleExpand.addEventListener('click', (e) => {
-        toggleExpand.classList.toggle('toggle-expand--open');
-        menu.classList.toggle('navigation-nav--open');
-        logo.classList.toggle('menu-logo--open');
-        littlePreMenu.classList.toggle('lower-block--open');
+    toggleExpand.forEach((el) => {
+      el.addEventListener('click', (e) => {
+        toggleExpand.forEach((toggle) =>
+          toggle.classList.toggle('toggle-expand--open'),
+        );
+        menu.forEach((m) => m.classList.toggle('navigation-nav--open'));
+        logo.forEach((l) => l.classList.toggle('menu-logo--open'));
+        searchIcon.forEach((si) => si.classList.toggle('search-icon--open-menu'));
+        littlePreMenu.forEach((lm) => lm.classList.toggle('little-pre-menu--open'));
+        pricingButton.forEach((pb) => pb.classList.toggle('button-see-pricing--open'));
+        socialLinks.forEach((sl) => sl.classList.toggle('social-links--open'));
         e.preventDefault();
       });
+    });
 
-      // Expose mobile sub menu on click.
-      Array.from(expandMenu).forEach((item) => {
-        item.addEventListener('click', (e) => {
-          const menuItem = e.currentTarget;
-          const subMenu = menuItem.nextElementSibling;
-
-          menuItem.classList.toggle('expand-sub--open');
-          subMenu.classList.toggle('main-menu--sub-open');
-        });
+    searchIcon.forEach((i) => {
+      i.addEventListener('click', (e) => {
+        searchIcon.forEach((icon) =>
+          icon.classList.toggle('search-icon--open'),
+        );
+        searchForm.forEach((ib) => ib.classList.toggle('search-block-form--open'));
+        e.preventDefault();
       });
-    }
+    });
   },
 };
