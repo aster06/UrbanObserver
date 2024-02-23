f2e5ee5a21dd6aba0d32c6c171cfe08af051f2fc
Bohdana Ostapiuk <danaostapiuk753@gmail.com>
2024-02-23 17:15:19 +0200

    #7453: Fixed navigation-menu js file and removed once jquery

--- web/themes/custom/urbanobserver/components/02-molecules/menus/navigation/navigation-menu.js
+++ web/themes/custom/urbanobserver/components/02-molecules/menus/navigation/navigation-menu.js
@@ -1,25 +1,17 @@
 Drupal.behaviors.mainMenu = {
   attach(context) {
     const toggleExpand = once('toggle-expand', '#toggle-expand', context);
-    const menu = once('navigation-nav', '#navigation-nav', context);
-    const logo = once('nav-logo', '#nav-logo', context);
-    const littlePreMenu = once('lower-block', '.little-pre-menu', context);
+    const navRegionMenu = once('nav-region', '.nav-region', context);
     const searchIcon = once('search-icon', '#search-icon', context);
     const searchForm = once('search-block-form', '#block-urbanobserver-search-form-wide', context);
-    const pricingButton = once('see-pricing', '.button-see-pricing', context);
-    const socialLinks = once('social-links', '#social-links', context);
 
     toggleExpand.forEach((el) => {
       el.addEventListener('click', (e) => {
         toggleExpand.forEach((toggle) =>
           toggle.classList.toggle('toggle-expand--open'),
         );
-        menu.forEach((m) => m.classList.toggle('navigation-nav--open'));
-        logo.forEach((l) => l.classList.toggle('menu-logo--open'));
+        navRegionMenu.forEach((a) => a.classList.toggle('nav-class--open'));
         searchIcon.forEach((si) => si.classList.toggle('search-icon--open-menu'));
-        littlePreMenu.forEach((lm) => lm.classList.toggle('little-pre-menu--open'));
-        pricingButton.forEach((pb) => pb.classList.toggle('button-see-pricing--open'));
-        socialLinks.forEach((sl) => sl.classList.toggle('social-links--open'));
         e.preventDefault();
       });
     });
