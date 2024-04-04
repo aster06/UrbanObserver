22e1b487adc16f43c1e5cd7b36984cb550967f1d
Bohdana Ostapiuk <danaostapiuk753@gmail.com>
2024-04-04 17:45:23 +0300

    #7768: Created plugin for CKeditor to change the font color and background color.

---
+++ web/modules/custom/ckeditor_custom/js/ckeditor5_plugins/buttonPlugin/src/buttonconfig.js
@@ -0,0 +1,69 @@
+import { icons } from 'ckeditor5/src/core';
+
+export const sizeOptions = {
+  large: {
+    label: 'Large',
+    icon: icons.large,
+    className: 'cke-large'
+  },
+  regular: {
+    label: 'Regular',
+    icon: icons.regular,
+    className: 'cke-regular'
+  },
+  small: {
+    label: 'Small',
+    icon: icons.small,
+    className: 'cke-small'
+  },
+};
+
+export const defaultSize = 'regular';
+
+export const colorOptions = {
+  blue: {
+    label: 'Blue',
+    className: 'ucb-link-button-blue'
+  },
+  black: {
+    label: 'Black',
+    className: 'ucb-link-button-black'
+  },
+  gray: {
+    label: 'Gray',
+    className: 'ucb-link-button-gray'
+  },
+  white: {
+    label: 'White',
+    className: 'ucb-link-button-white'
+  },
+  gold: {
+    label: 'Gold',
+    className: 'ucb-link-button-gold'
+  }
+};
+
+export const textColorOptions = {
+  blue: {
+    label: 'Blue',
+    className: 'ucb-text-link-button-blue'
+  },
+  black: {
+    label: 'Black',
+    className: 'ucb-text-link-button-black'
+  },
+  gray: {
+    label: 'Gray',
+    className: 'ucb-text-link-button-gray'
+  },
+  white: {
+    label: 'White',
+    className: 'ucb-text-link-button-white'
+  },
+  gold: {
+    label: 'Gold',
+    className: 'ucb-text-link-button-gold'
+  }
+};
+
+export let textDefaultColor = 'black';
